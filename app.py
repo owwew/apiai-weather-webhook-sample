d429a277e9d8d71256261a43d18be76974f106c2
owwew <kasmiamine25@gmail.com>
2017-06-14 12:38:57 +0200

    Create app.py

--- app.py
+++ app.py
@@ -93,14 +93,14 @@ def locu_serch(query):
     url = "http://maps.googleapis.com/maps/api/geocode/json?"
     address=query
     final_url = url + "&address=" + address #url + le mot "address=" + "la ville" a chercher
-    #response = urllib2.urlopen(final_url)
-    #data =  json.loads(response.read())     #"la fonction google marche tres bien" 
-    #add = "adresse : "   
-    #x=0
-    #for item in data["results"][0]["address_components"]:
-        #add = add + "\n" + data["results"][0]["address_components"][x]["long_name"]
-        #x=x+1
-    return final_url#response#address #add
+    response = urllib2.urlopen(final_url)
+    data =  json.loads(response.read())     #"la fonction google marche tres bien" 
+    add = "adresse : "   
+    x=0
+    for item in data["results"][0]["address_components"]:
+        add = add + "\n" + data["results"][0]["address_components"][x]["long_name"]
+        x=x+1
+    return add
     
     
 
