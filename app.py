5a2c51d932f1747a6f3bbeda519f16672e273e0a
owwew <kasmiamine25@gmail.com>
2017-06-26 17:27:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -101,7 +101,6 @@ def dojcmssearch(req):
     xmldoc = ET.fromstring(get_web)
     f = StringIO.StringIO(get_web)
     test = etree.parse(f)
-    titre #CIIC
     for titre in test.find("data").xpath("/dataset/data/field[@name='title']/text()"):
         print (titre) 
     return {
